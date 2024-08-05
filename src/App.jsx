e483317dd17ba5a418c8220a6a1aa65736ebd5a6
prasad <prasadneroolkar90@gmail.com>
2024-08-05 21:21:12 +0530

    css initiated

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,24 @@ import SidebarContact from "./components/SidebarContact";
 function App() {
   return (
     <>
-      <Sidebar>
-        <SideInfo />
-        <SidebarContact />
-      </Sidebar>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-12">
+            <Sidebar>
+              <SideInfo />
+              <SidebarContact />
+            </Sidebar>
+          </div>
+          <div className="col-lg-8 col-12">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Quibusdam aperiam commodi quisquam aliquid sequi dolore eveniet
+              itaque ullam praesentium id culpa soluta vitae, quod accusamus
+              iure labore corrupti fugit libero!
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
